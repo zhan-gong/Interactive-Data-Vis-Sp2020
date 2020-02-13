e0b3d12818827bed8be8910584783c4a1cf931e1
zhan-gong <60577804+zhan-gong@users.noreply.github.com>
2020-02-13 12:46:47 -0500

    as2

---
+++ AS2_horizontal/main.js
@@ -0,0 +1,68 @@
+// data load
+// reference for d3.autotype: https://github.com/d3/d3-dsv#autoType
+d3.csv("../../data/CoronaVirus.csv", d3.autoType).then(data => {
+  console.log(data);
+
+  /** CONSTANTS */
+  // constants help us reference the same values throughout our code
+  const width = window.innerWidth * 0.9,
+    height = window.innerHeight,
+    paddingInner = 0.2,
+    margin = { top: 20, bottom: 40, left: 100, right: 100 };
+
+  /** SCALES */
+  // reference for d3.scales: https://github.com/d3/d3-scale
+  const yScale = d3
+    .scaleBand()
+    .domain(data.map(d => d.City))
+    .range([margin.top, height - margin.bottom])
+    .paddingInner(paddingInner);
+
+  const xScale = d3
+    .scaleLinear()
+    .domain([d3.min(data, d => d.Count), d3.max(data, d => d.Count)])
+    .range([width - margin.right, 0]);
+
+  // reference for d3.axis: https://github.com/d3/d3-axis
+  // const yAxis = d3.axisBottom(xScale).ticks(data.length);
+  const yAxis = d3.axisLeft(yScale)
+
+  /** MAIN CODE */
+  const svg = d3
+    .select("#d3-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  // append rects
+  const rect = svg
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("y", d => yScale(d.City) + margin.top)
+    .attr("x", margin.left)
+    .attr("width", d => width - margin.right - xScale(d.Count))
+    .attr("height", yScale.bandwidth())
+    .attr("fill", "steelblue")
+
+  // append text
+  const text = svg
+    .selectAll("text")
+    .data(data)
+    .join("text")
+    .attr("class", "label")
+    .attr("y", d => yScale(d.City) + margin.top)
+    .attr("x", margin.left)
+    // this allows us to position the text in the center of the bar
+    // .attr("x", d => yScale(d.City) + (yScale.bandwidth() / 2))
+    // .attr("y", d => xScale(d.Count))
+    .text(d => d.Count)
+    .attr("dx", "2.34em")
+    .attr("dy", "1.5em");
+
+  svg
+    .append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate("+ margin.left +", "+ margin.top +")")
+    .call(yAxis);
+});
